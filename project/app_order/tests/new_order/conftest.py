f493f47a4736960554d719df274ebbf53c3f90ec
Vitaliy Sazonov <sazonov89vit@gmail.com>
2023-07-17 17:37:33 +0300

    add tests for delete_new_order_iten func

--- project/app_order/tests/new_order/conftest.py
+++ project/app_order/tests/new_order/conftest.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 import pytest
 
 from mixer.backend.django import mixer
+
+from app_order.services.new_order import update_or_create_new_order
 # from project.test.mixer import mixer
 
 
@@ -25,4 +27,13 @@ def menu_item():
 def an_menu_item():
     return mixer.blend("app_menu.menuitem")
 
+@pytest.fixture
+def order_with_one_item(menu_item, user):
+    order = update_or_create_new_order(user=user, item=menu_item)[0][0]
+    return order
 
+@pytest.fixture
+def order_with_two_items(menu_item, an_menu_item, user):
+    update_or_create_new_order(user=user, item=menu_item)[0][0]
+    order = update_or_create_new_order(user=user, item=an_menu_item)[0][0]
+    return order
